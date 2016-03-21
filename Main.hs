812b1c2687bc518f6a78bdbc735d6ebd0d1778a1
Dmitry Budyak <dmitry.budyak@ligastavok.ru>
2016-03-21 21:35:27 +0300

    ada

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import Debug.Trace
+import Data.Function
 
 twoDigits2Int :: Char -> Char -> Int
 twoDigits2Int x y  | isDigit x && isDigit y = read (x:(y:[]))::Int | otherwise = 100
@@ -20,31 +21,23 @@ fib n
 	| n > 1 = fib (n - 1) + fib (n - 2) 
 	| n < -1 = fib (n + 2) - fib (n + 1)
 
-
-
 fib' :: Integer -> Integer
-fib' n = helper n 1 1
+fib' n = helper 0 1 n
 
 helper :: Integer -> Integer -> Integer -> Integer
---helper 1 f1 f2 = f1
---helper 0 f1 f2 = 0
---helper (-1) f1 f2 | f2-f1 == 0 =  | otherwise =  f2-f1
-helper n f1 f2
-	| n == 1 = f1
-	| n == 0 = 0
---	| n == -1 && f2 - f1 == 0 = helper (n-1) f1 (f1-f2)
-	| n == -1 = f2 - f1
-	| n > 1 = helper (n-1) f2 (f1+f2)
-	| n < -1 = helper (n+1) f2 (f1-f2)
+helper curr prev n 
+	| n == 0 = curr
+	| n > 0  = helper (curr + prev) curr (n - 1)
+	| n < 0	 = helper prev (curr - prev) (n + 1)
 
+--[1,2,3,3,2,-1,-5,-10,-13,-13,-6,7,27,46,59,51,18,-49,-133,-218,-253]
+seqA :: Integer -> Integer
+seqA n = seqA' 0 3 2 1 n 0
+seqA' :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+seqA' curr prev1 prev2 prev3 n k | k < n = seqA' (prev1 + prev2 - 2 * prev3) curr prev1 prev2 n (k+1) | n == k = curr
 
---func :: (Num a) => a -> a -> a -> a
---func n c b a = func (n-1) q b a
---	 where q= c+p-
---	                  
---
---f :: (Num a) => a -> a
---f k = func k 3 2 1
+getSecondFrom :: t1 -> t2 -> t3 -> t2
+getSecondFrom x y z = y
 
-
-
+on3 :: (b -> b -> b -> c) -> (a -> b) -> a -> a -> a -> c
+on3 op f x y z = 	
